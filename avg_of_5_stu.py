a8256f9c034113a77ebebd80c763dad2cc24b6ee
test-git-ucp <abdullahnawaz8287@gmail.com>
2025-09-20 08:55:29 +0500

    Add files via upload

---
+++ avg_of_5_stu.py
@@ -0,0 +1,18 @@
+
+
+n=[]
+
+
+n1=int(input("enter the masrks 1: "))
+n2=int(input("enter the masrks 2: "))
+n3=int(input("enter the masrks 3: "))
+n4=int(input("enter the masrks 4: "))
+n5=int(input("enter the masrks 5: "))
+n.append(n1)
+n.append(n2)
+n.append(n3)
+n.append(n4)
+n.append(n5)
+avg=((n1+n2+n3+n4+n5)/5)
+print(n)
+print("average of 5 student is : ",avg)
