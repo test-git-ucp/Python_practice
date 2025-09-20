30d4dc5af20b2fa1688f5ecf7000e2d66b5ce3b1
test-git-ucp <abdullahnawaz8287@gmail.com>
2025-09-20 09:05:39 +0500

    Add files via upload

---
+++ text.py
@@ -0,0 +1,8 @@
+# f=open("hello.txt","w+")
+# s=f.write("miasb har bnda ak warga nhi hnda\n tu smjha nhi sii mia sb tanu kinni wari akha ha \ntu chabal ha\nsuna phr ki rah gya tera kol\n reh gya na kalaa .")
+
+# f.seek(0)
+# g=f.readlines()
+# print(g)
+# print(s)
+# f.close()
